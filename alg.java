ffabd75308e418e3acffa5be6817e1b7abfa90c1
ShaneTheManiac <khb@duck.com>
2022-06-10 23:49:04 -0400

    updated the algorithm class
    made the generateGrid method and constructor method, not really anything big, just wanting to keep you guys updated

--- alg.java
+++ alg.java
@@ -1,9 +1,36 @@
+import java.util.Random;
+
 public class alg {
     private final int SIZE;
     private boolean[][] occupied;
+    private char[][] grid;
 
-    public alg(int SIZE) {
+    private alg(int SIZE) {
+        // constructor method, makes all the variables and calls generateGrid();
         this.SIZE = SIZE;
         occupied = new boolean[SIZE][SIZE];
+        grid = new char[SIZE][SIZE];
+        generateGrid();
+    }
+
+    private void generateGrid() {
+        // makes grid for the first time randomly
+        String[] dice = new String[]{
+                "AAAFRS", "AAEEEE", "AAFIRS", "ADENNN", "AEEEEM",
+                "AEEGMU", "AEGMNN", "AFIRSY", "BJKQXZ", "CCNSTW",
+                "CEIILT", "CEILPT", "CEIPST", "DDLNOR", "DHHLOR",
+                "DHHNOT", "DHLNOR", "EIIITT", "EMOTTT", "ENSSSU",
+                "FIPRSY", "GORRVW", "HIPRRY", "NOOTUW", "OOOTTU"};
+
+        Random rand = new Random();
+
+        for (int row = 0; row < SIZE; row++) {
+            for (int column = 0; column < SIZE; column++) {
+                int index = (row * SIZE + column) % 25;
+                // index will iterate through dice array and loop back to start when out of bounds
+                int randomInt = rand.nextInt(6);
+                grid[row][column] = dice[index].charAt(randomInt);
+            }
+        }
     }
 }
