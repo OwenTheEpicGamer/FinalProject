ab4d2f283b3cb214463822b0afab6a7606dc581b
ShaneTheManiac <khb@duck.com>
2022-06-11 01:56:50 -0400

    Prototype wordList Generation alg
    THIS PROBABLY DOES NOT WORK AND IS LIKELY UINSTABLE, NOT SUITABLE FOR PRODUCTION ENVIRONMENT, USE AT YOUR OWN RISK

--- alg.java
+++ alg.java
@@ -1,9 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
 
 public class alg {
     private final int SIZE;
     private boolean[][] occupied;
     private char[][] grid;
+    private Set<String> dict;
+    private Set<String> wordList;
 
     private alg(int SIZE) {
         // constructor method, makes all the variables and calls generateGrid();
@@ -11,6 +18,9 @@ public class alg {
         occupied = new boolean[SIZE][SIZE];
         grid = new char[SIZE][SIZE];
         generateGrid();
+        generateDict();
+        generateWordlist();
+
     }
 
     private void generateGrid() {
@@ -33,4 +43,54 @@ public class alg {
             }
         }
     }
+
+    private void generateDict() {
+        dict = new HashSet<String>();
+
+        try {
+            File dictFile = new File("dictionary.txt");
+            Scanner dictReader = new Scanner(dictFile);
+            dict.add(dictReader.nextLine());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void generateWordlist() {
+        wordList = new HashSet<String>();
+        for (int row = 0; row < SIZE; row++) {
+            for (int column = 0; column < SIZE; column++) {
+                String startLetter = String.valueOf(grid[row][column]);
+                checkPossibilities(startLetter, row, column);
+            }
+        }
+
+    }
+
+    private void checkPossibilities(String word, int row, int column) {
+
+        if (dict.contains(word)) {
+            wordList.add(word);
+        }
+
+        // 2d array holding coords of all surrounding cells
+        int[][] neighbours = {
+                {row + 1, column}, {row - 1, column},
+                {row, column + 1}, {row, column - 1},
+                {row + 1, column + 1}, {row - 1, column - 1},
+                {row + 1, column - 1}, {row - 1, column + 1}};
+
+        for (int[] coord : neighbours) {
+            // next means the value for the next pass
+            int nextRow = coord[0];
+            int nextColumn = coord[1];
+            char nextLetter = grid[nextRow][nextColumn];
+
+            checkPossibilities(word + nextLetter, nextRow, nextColumn);
+        }
+    }
+
 }
+
+
+
