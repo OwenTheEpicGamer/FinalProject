51ea519675f5f486d8609a8d60e146775681bca9
ShaneTheManiac <khb@duck.com>
2022-06-10 23:20:03 -0400

    created program files
    created program files, main class, added some started code to the algorithm, will soon finish main class and alg code

--- README.md
+++
@@ -1,8 +0,0 @@
-# Team Message
-
-roles:
-
-kian: **alg**
-sarina: **gui**
-
-owen and ryan: **do shit**
---
+++ alg.java
@@ -0,0 +1,9 @@
+public class alg {
+    private final int SIZE;
+    private boolean[][] occupied;
+
+    public alg(int SIZE) {
+        this.SIZE = SIZE;
+        occupied = new boolean[SIZE][SIZE];
+    }
+}
